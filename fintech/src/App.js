0133326b0661cd6b70c4cd79111716b21845878e
eun921 <leekoeun5@gmail.com>
2021-08-24 14:00:34 +0900

    210824

---
+++ fintech/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header title="메인화면"></Header>
+        <Header title="거래내역조회"></Header>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ fintech/src/component/Header.js
@@ -0,0 +1,14 @@
+//rafce
+
+import React from 'react'
+
+const Header = ({title}) => {
+    return (
+        <div>
+            <p>{title}</p>
+            
+        </div>
+    )
+}
+
+export default Header;
