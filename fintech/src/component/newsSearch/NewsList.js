e64ac449af8148fe7cc5095bd1a9bb250a37a2b6
eun921 <leekoeun5@gmail.com>
2021-08-25 14:42:56 +0900

    210825

--- fintech/src/component/newsSearch/NewsList.js
+++ fintech/src/component/newsSearch/NewsList.js
@@ -1,10 +1,16 @@
 import React from "react";
+import NewsListItem from "./NewsListItem";
 
 const NewsList = ({ searchResult }) => {
   return (
     <div>
       {searchResult.map((news) => {
-        return <p>{news.author}</p>;
+        return <NewsListItem 
+        title={news.title} 
+        contents={news.contents} 
+        url={news.url}
+        key={news.index}
+        ></NewsListItem>;
       })}
     </div>
   );
--- fintech/src/component/newsSearch/NewsListItem.js
+++ fintech/src/component/newsSearch/NewsListItem.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const NewsListItem = () => {
+const NewsListItem = ({title,contents,url}) => {
   return <div>
-      <p>제목 : </p>
-      <p>내용 : </p>
-      <a href={}> 링크 : </a>
+      <p>제목 : {title}</p>
+      <p>작성자 : {contents}</p>
+      <a href={url}> 기사로 이동하기 </a>
   </div>;
 };
 
