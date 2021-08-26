cd840ad8316469c3442a8f24563dabf3d74becd7
eun921 <leekoeun5@gmail.com>
2021-08-26 16:06:30 +0900

    거래내역 조회 API

--- fintech/src/pages/Balance.js
+++ fintech/src/pages/Balance.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
 import BalanceCard from "../component/balance/BalanceCard";
+import TransactionList from "../component/balance/TransactionList";
 import Header from "../component/Header";
 import axios from "axios";
 
 const Balance = () => {
   const { search } = useLocation();
   const { fintechUseNo } = queryString.parse(search);
-  const [balance, setBalance] = useState();
+  const [balance, setBalance] = useState({});
+  const [transaction, setTransaction] = useState([]);
+  console.log(fintechUseNo);
 
   const genTransId = () => {
     let countnum = Math.floor(Math.random() * 1000000000) + 1;
@@ -18,6 +21,7 @@ const Balance = () => {
 
   useEffect(() => {
     getBalanceData();
+    getTransactionList();
   }, []);
 
   const getBalanceData = () => {
@@ -36,13 +40,44 @@ const Balance = () => {
     };
     axios(option).then(({ data }) => {
       console.log(data);
+      setBalance(data);
+    });
+  };
+
+  const getTransactionList = () => {
+    const accessToken = localStorage.getItem("accessToken");
+    const option = {
+      method: "GET",
+      url: `v2.0/account/transaction_list/fin_num`,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: {
+        bank_tran_id: genTransId(),
+        fintech_use_num: fintechUseNo,
+        inquiry_type: "A",
+        inquiry_base: "D",
+        from_date: "20190101",
+        to_date: "20210826",
+        sort_order: "D",
+        tran_dtime: "20210826150000",
+      },
+    };
+    axios(option).then(({ data }) => {
+      console.log(data);
+      setTransaction(data.res_list);
     });
   };
 
   return (
     <div>
       <Header title="잔액조회"></Header>
-      <BalanceCard></BalanceCard>
+      <BalanceCard
+        bankName={balance.bank_name}
+        fintechNo={balance.fintech_use_num}
+        balance={balance.balance_amt}
+      ></BalanceCard>
+      <TransactionList transactionList={transaction}></TransactionList>
     </div>
   );
 };
